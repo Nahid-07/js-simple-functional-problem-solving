62ca96c1028cf2515ea9f9f12aafd49344308a50
Nahid-07 <nahidasif1998@gmail.com>
2022-08-05 14:54:52 +0600

    #

--- hoursToMinute.js
+++ hoursToMinute.js
@@ -1,3 +1,6 @@
+// Write a function that will take hour as the input parameter and will
+// convert it into minutes and will return the result in minutes.
+
 function getMinute(hours){
     let result = hours * 60;
     return result;
